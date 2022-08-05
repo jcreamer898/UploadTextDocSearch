a72bcdca9a438ff56c270b278c682bf30c28ef13
VWithun <73565294+VWithun@users.noreply.github.com>
2022-08-05 18:57:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-
 const searchBtn = document.getElementById("searchBtn");
-const output = document.querySelector(".output");
+let output = document.querySelector(".output");
+let changeText = document.getElementById("changeText");
+const saveBtn = document.getElementById("saveBtn");
+let outPutTxt = String(output.innerText);
 
+//upload and input text doc:
 function textReader() {
- 
   const [file] = document.querySelector("input[type=file]").files;
 
   const fileReader = new FileReader();
@@ -18,18 +20,54 @@ function textReader() {
 
   if (file) {
     fileReader.readAsText(file);
-    searchBtn.addEventListener("click", searchText);
-    
+      
   }
-  
-
 }
 
-  function searchText() {
-    const searchBar = document.getElementById("searchBar").value;
-   
-    console.log(searchBar);
+// Replace occurance
+
+document.addEventListener("DOMContentLoaded", function () {
+  const replaceBtn = document.getElementById("replaceBtn");
+  replaceBtn.addEventListener("click", findReplace);
+  searchBtn.addEventListener("click", findOccurance);
+ 
+});
+
+const findReplace = (e) => {
+  e.preventDefault();
+
+  let searchBar = document.getElementById("searchBar").value;
+  let replaceBar = document.getElementById("replaceBar").value;
+
+  let text = document.querySelector(".output");
+  
+
+
+  if (
+    typeof text.textContent === "string" ||
+    text.textContent instanceof String
+    
+  ) {
+    text.textContent = text.textContent.replaceAll(searchBar, replaceBar);
+  } else {
+    console.log("not string");
   }
 
+ 
+};
+  
+const findOccurance = ()=>{
+let search = document.getElementById("searchBar").value;
+let textOccurance = document.querySelector(".output").textContent;
+let pOutput = document.getElementById("occurance");
+let count = (textOccurance.split(search).length -1);
+pOutput.innerHTML =`Number of occurances = ${count}`
 
 
+  console.log(count);
+}
+
+// const saveText =()=>{
+//  const changedFile = document.getElementById("changedFile");
+//  saveAs(changedFile);
+// }
